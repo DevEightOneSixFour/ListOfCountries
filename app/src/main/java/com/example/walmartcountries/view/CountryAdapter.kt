7d1d8dfc530124e5c4df54000b91f80b7b5101c4
DevEightOneSixFour <sebrown@enhanceit.com>
2022-06-10 15:06:27 -0400

    no sealed class

--- app/src/main/java/com/example/walmartcountries/view/CountryAdapter.kt
+++ app/src/main/java/com/example/walmartcountries/view/CountryAdapter.kt
@@ -1,26 +1,20 @@
 package com.example.walmartcountries.view
 
-import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.example.walmartcountries.R
 import com.example.walmartcountries.databinding.CountryListItemBinding
+import com.example.walmartcountries.databinding.HeaderListItemBinding
 import com.example.walmartcountries.model.CountryItem
 
 class CountryAdapter(
-    private val countries: MutableList<CountryItem> = mutableListOf()
-    ): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+    private val countries: MutableList<Any> = mutableListOf()
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     inner class CountryViewHolder(
         private val binding: CountryListItemBinding
-        ): RecyclerView.ViewHolder(binding.root) {
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(item: CountryItem) {
             binding.apply {
                 tvCountryName.text = item.getNameAndRegion()
@@ -28,56 +22,71 @@ class CountryAdapter(
                 tvCountryCode.text = item.code
                 Glide.with(ivFlag)
                     .load(item.getFlagImage())
-                    .listener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(
-                            e: GlideException?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            pbLoading.visibility = View.GONE
-                            return false
-                        }
-
-                        override fun onResourceReady(
-                            resource: Drawable?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            dataSource: DataSource?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            pbLoading.visibility = View.GONE
-                            return false
-                        }
-
-                    })
+                    .placeholder(R.drawable.giphy)
                     .into(ivFlag)
             }
         }
     }
 
-    fun setCountryList(newList: List<CountryItem>) {
-        countries.apply {
-            clear()
-            addAll(newList)
-            sortBy { it.name }
+    inner class HeaderViewHolder(
+        private val binding: HeaderListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(letter: Char) {
+            binding.headerLetter.text = letter.toString()
         }
-        notifyItemRangeChanged(0,itemCount)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        CountryViewHolder(
-            CountryListItemBinding.inflate(
-                LayoutInflater.from(
-                    parent.context
-                ),
-                parent,
-                false
-            )
-        )
+    companion object {
+        const val HEADER = 1
+        const val COUNTRY = 2
+    }
 
-    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        holder.onBind(countries[position])
+    fun setCountryList(newList: List<CountryItem>) {
+        var currentLetter = ' '
+        newList.sortedBy { it.name }.forEach { item ->
+            item.name.first().apply {
+                if (this != currentLetter) {
+                    countries.add(this)
+                    currentLetter = this
+                }
+                countries.add(item)
+            }
+        }
+        notifyItemRangeChanged(0, itemCount)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            HEADER -> HeaderViewHolder(
+                HeaderListItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+            else -> {
+                CountryViewHolder(
+                    CountryListItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is HeaderViewHolder -> holder.onBind(countries[position] as Char)
+            is CountryViewHolder -> holder.onBind(countries[position] as CountryItem)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (countries[position]) {
+            is Char -> HEADER
+            else -> COUNTRY
+        }
     }
 
     override fun getItemCount() = countries.size
